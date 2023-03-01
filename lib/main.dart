36f0e1b15ebfb2abd5cad642823efc77e6f2fd83
mohiitt <mohitbarade1@gmail.com>
2023-03-01 22:29:23 +0530

    finalcommit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'Screens/loginscreen.dart';
+import 'firebase_options.dart';
 
-import 'loginscreen.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  await Firebase.initializeApp(options:
+  DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:loginscreen(),
-      );
-
+      home: loginscreen(),
+    );
   }
 }
-
